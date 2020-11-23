13e55f2118ea6228d27228c0c2c5de60b28728d9
osy1223 <65882602+osy1223@users.noreply.github.com>
2020-11-23 19:44:05 +0900

    Update kosdaq_data.py

--- kosdaq_data.py
+++ kosdaq_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pandas import DataFrame
 
 # csv 파일 불러오기 (인코딩 주의)
-kosdaq = pd.read_csv('./data/코스닥.csv',
+kosdaq = pd.read_csv('./data/csv/코스닥.csv',
             header=0, index_col=0, sep=',', encoding='cp949')
 
 print('kosdaq:',kosdaq)
@@ -25,10 +25,3 @@ print(kosdaq)
 # [620 rows x 3 columns]
 
 
-'''
-#콤마 제거 후 문자를 정수로 변환
-for i in range(len(kosdaq.index)):
-    for j in range(len(kosdaq.iloc[i])):
-        kosdaq.iloc[i,j] = int(kosdaq.iloc[i,j].replace('.',''))
-print(kosdaq)
-'''
